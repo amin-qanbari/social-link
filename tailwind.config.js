869938670ae6126bd99c095fc6382107e1d55f3a
amin <aminqanbarii1992@gmail.com>
2024-07-30 22:19:36 +0330

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,48 @@
+const config = {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
+  prefix: "",
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      colors: {
+        white: {
+          1: "#FFFFFF",
+          2: "rgba(255, 255, 255, 0.72)",
+          3: "rgba(255, 255, 255, 0.4)",
+          4: "rgba(255, 255, 255, 0.64)",
+          5: "rgba(255, 255, 255, 0.80)",
+        },
+        black: {
+          1: "#15171C",
+          2: "#222429",
+          3: "#101114",
+          4: "#252525",
+          5: "#2E3036",
+          6: "#24272C",
+        },
+        orange: {
+          1: "#F97535",
+        },
+        gray: {
+          1: "#71788B",
+        },
+      },
+      backgroundImage: {},
+      keyframes: {},
+    },
+  },
+};
+
+export default config;
